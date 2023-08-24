8944deca831edfb33e51b62353430db957fba722
KRochaS <karinerocha.s@hotmail.com>
2023-08-23 22:48:21 -0300

    Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,24 @@
-import Document from 'next/document';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// eslint-disable-next-line prettier/prettier
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable prettier/prettier */
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+          <link rel="shortcut icon" href="/favicon.png" type="img/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
